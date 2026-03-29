5c2f78ab5e11877ffbdf098b95b1204d954bb0ec
Frank Webster <frankwwebster@gmail.com>
2026-03-29 15:04:56 +0100

    Add visual polish pass for tutorial room and HUD

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -49,66 +49,74 @@ export default class Game extends Phaser.Scene {
   }
 
   drawFloor() {
-    this.add.text(20, 20, 'Escape from Penwortham', {
-      fontFamily: 'Arial',
-      fontSize: '28px',
-      color: '#1a1a1a'
-    })
-
-    this.add.text(20, 56, 'Collect gold, bonk the Hall Monitor, grab the key, then head for the door.', {
-      fontFamily: 'Arial',
-      fontSize: '16px',
-      color: '#2b2b2b'
-    })
+    this.add.rectangle(400, 46, 760, 68, 0x254f6c, 0.96).setStrokeStyle(3, 0xf7f0c6)
+    this.add.rectangle(400, 46, 728, 44, 0x3a7091, 0.7).setStrokeStyle(2, 0xffffff, 0.15)
 
     this.add.rectangle(
       this.roomLeft + this.roomWidth / 2,
       this.roomTop + this.roomHeight / 2,
       this.roomWidth,
       this.roomHeight,
-      0xe9ddb8
-    )
+      0xf4e4b8
+    ).setStrokeStyle(6, 0xc6a76c)
 
     const tileSize = 40
 
     for (let y = this.roomTop; y < this.roomBottom; y += tileSize) {
       for (let x = this.roomLeft; x < this.roomRight; x += tileSize) {
         const useAltColor = ((x - this.roomLeft) / tileSize + (y - this.roomTop) / tileSize) % 2 === 0
-        const color = useAltColor ? 0xe7dab0 : 0xdece9e
+        const color = useAltColor ? 0xf1e2b7 : 0xe8d49f
 
         this.add.rectangle(x + tileSize / 2, y + tileSize / 2, tileSize, tileSize, color)
-          .setStrokeStyle(1, 0xd1c089, 0.35)
+          .setStrokeStyle(1, 0xd6bf7a, 0.45)
       }
+    }
+
+    for (let y = this.roomTop + 20; y < this.roomBottom; y += 80) {
+      this.add.rectangle(this.roomLeft + this.roomWidth / 2, y, this.roomWidth - 28, 4, 0xffffff, 0.12)
     }
   }
 
   drawClassroomDecor() {
-    this.add.rectangle(400, 78, 210, 22, 0x2f6b3d)
-    this.add.rectangle(400, 78, 214, 26)
+    this.add.rectangle(400, 78, 240, 28, 0x2f6b3d)
+    this.add.rectangle(400, 78, 246, 34)
       .setStrokeStyle(3, 0x704d2d)
       .setFillStyle(0x000000, 0)
 
-    this.add.text(333, 68, 'TODAY: ESCAPE!', {
+    this.add.text(323, 67, 'TODAY: ESCAPE!', {
       fontFamily: 'Arial',
-      fontSize: '16px',
+      fontSize: '18px',
       color: '#f4f7f0'
     })
 
-    this.add.rectangle(165, 150, 70, 40, 0xb78654).setStrokeStyle(2, 0x6e4d2f)
-    this.add.rectangle(245, 150, 70, 40, 0xb78654).setStrokeStyle(2, 0x6e4d2f)
-    this.add.rectangle(165, 205, 70, 40, 0xb78654).setStrokeStyle(2, 0x6e4d2f)
-    this.add.rectangle(245, 205, 70, 40, 0xb78654).setStrokeStyle(2, 0x6e4d2f)
+    this.add.rectangle(165, 150, 70, 40, 0xc59155).setStrokeStyle(2, 0x6e4d2f)
+    this.add.rectangle(245, 150, 70, 40, 0xc59155).setStrokeStyle(2, 0x6e4d2f)
+    this.add.rectangle(165, 205, 70, 40, 0xc59155).setStrokeStyle(2, 0x6e4d2f)
+    this.add.rectangle(245, 205, 70, 40, 0xc59155).setStrokeStyle(2, 0x6e4d2f)
+
+    this.add.rectangle(165, 132, 58, 6, 0xe7c79c, 0.9)
+    this.add.rectangle(245, 132, 58, 6, 0xe7c79c, 0.9)
+    this.add.rectangle(165, 187, 58, 6, 0xe7c79c, 0.9)
+    this.add.rectangle(245, 187, 58, 6, 0xe7c79c, 0.9)
 
     this.add.rectangle(585, 180, 110, 55, 0xc99b6b).setStrokeStyle(2, 0x6e4d2f)
+    this.add.rectangle(585, 152, 90, 10, 0xe5c798, 0.9)
     this.add.text(545, 170, 'Teacher Desk', {
       fontFamily: 'Arial',
       fontSize: '14px',
       color: '#2c1b12'
     })
+
+    this.add.rectangle(120, 360, 38, 90, 0xb34d4d).setStrokeStyle(2, 0x6e2323)
+    this.add.text(103, 328, 'BOOKS', {
+      fontFamily: 'Arial',
+      fontSize: '12px',
+      color: '#5f1d1d'
+    })
   }
 
   createWalls() {
-    this.wallColor = 0x8a8f96
+    this.wallColor = 0x6f7f93
     this.walls = [
       {
         x: this.roomLeft,
@@ -149,7 +157,16 @@ export default class Game extends Phaser.Scene {
         wall.width,
         wall.height,
         this.wallColor
-      ).setStrokeStyle(2, 0x5d6166)
+      ).setStrokeStyle(3, 0xe8edf1)
+
+      this.add.rectangle(
+        wall.x + wall.width / 2,
+        wall.y + wall.height / 2,
+        wall.width - 6,
+        wall.height - 6,
+        0xffffff,
+        0.08
+      )
     })
   }
 
@@ -173,7 +190,7 @@ export default class Game extends Phaser.Scene {
       this.exit.y,
       this.exit.width,
       this.exit.height,
-      0x5c3b22
+      0x58341d
     )
 
     this.exitDoor = this.add.rectangle(
@@ -186,16 +203,24 @@ export default class Game extends Phaser.Scene {
 
     this.exitHandle = this.add.circle(this.exit.x + 2, this.exit.y, 3, 0xffd54f)
 
-    this.add.text(this.roomRight - 60, this.roomTop + 286, 'EXIT', {
+    this.exitWindow = this.add.rectangle(this.exit.x - 3, this.exit.y - 24, 9, 18, 0x8fc8e8, 0.8)
+      .setStrokeStyle(1, 0x2c5b73)
+
+    this.exitSign = this.add.rectangle(this.roomRight - 55, this.roomTop + 286, 66, 20, 0xf8f2cf)
+      .setStrokeStyle(2, 0x7a6940)
+
+    this.exitSignText = this.add.text(this.roomRight - 76, this.roomTop + 278, 'LOCKED', {
       fontFamily: 'Arial',
       fontSize: '14px',
-      color: '#1a1a1a'
+      color: '#6b2a2a'
     })
+
+    this.updateDoorLook()
   }
 
   createCoins() {
-    this.coin = this.add.circle(280, 300, 11, 0xffd54f).setStrokeStyle(3, 0xd49d00)
-    this.coin2 = this.add.circle(585, 425, 11, 0xffd54f).setStrokeStyle(3, 0xd49d00)
+    this.coin = this.createCoin(280, 300)
+    this.coin2 = this.createCoin(585, 425)
 
     this.tweens.add({
       targets: [this.coin, this.coin2],
@@ -208,16 +233,28 @@ export default class Game extends Phaser.Scene {
     })
   }
 
+  createCoin(x, y) {
+    const coin = this.add.container(x, y)
+    const glow = this.add.circle(0, 0, 16, 0xfff1a8, 0.28)
+    const outer = this.add.circle(0, 0, 11, 0xffd54f).setStrokeStyle(3, 0xc88a00)
+    const inner = this.add.circle(0, 0, 6, 0xffef99)
+    const shine = this.add.rectangle(-3, -3, 6, 2, 0xffffff, 0.7).setAngle(-25)
+
+    coin.add([glow, outer, inner, shine])
+    return coin
+  }
+
   createKey() {
     this.key = this.add.container(505, 245)
 
-    const keyHead = this.add.circle(0, 0, 9, 0x6cc6ff).setStrokeStyle(3, 0x1f6f99)
+    const keyGlow = this.add.circle(0, 0, 18, 0xa9f0ff, 0.28)
+    const keyHead = this.add.circle(0, 0, 9, 0x66d7ff).setStrokeStyle(3, 0x0b6f95)
     const keyHole = this.add.circle(0, 0, 3, 0xe9ddb8)
-    const keyStem = this.add.rectangle(16, 0, 24, 6, 0x6cc6ff).setStrokeStyle(2, 0x1f6f99)
-    const keyToothTop = this.add.rectangle(24, -5, 5, 7, 0x6cc6ff).setStrokeStyle(2, 0x1f6f99)
-    const keyToothBottom = this.add.rectangle(18, 5, 5, 7, 0x6cc6ff).setStrokeStyle(2, 0x1f6f99)
+    const keyStem = this.add.rectangle(16, 0, 24, 6, 0x66d7ff).setStrokeStyle(2, 0x0b6f95)
+    const keyToothTop = this.add.rectangle(24, -5, 5, 7, 0x66d7ff).setStrokeStyle(2, 0x0b6f95)
+    const keyToothBottom = this.add.rectangle(18, 5, 5, 7, 0x66d7ff).setStrokeStyle(2, 0x0b6f95)
 
-    this.key.add([keyHead, keyHole, keyStem, keyToothTop, keyToothBottom])
+    this.key.add([keyGlow, keyHead, keyHole, keyStem, keyToothTop, keyToothBottom])
 
     this.tweens.add({
       targets: this.key,
@@ -243,41 +280,23 @@ export default class Game extends Phaser.Scene {
       direction: 1
     }
 
-    this.hallMonitorShadow = this.add.ellipse(
-      this.hallMonitor.x,
-      this.hallMonitor.y + 15,
-      26,
-      10,
-      0x000000,
-      0.2
-    )
-
-    this.hallMonitorBody = this.add.rectangle(
-      this.hallMonitor.x,
-      this.hallMonitor.y,
-      this.hallMonitor.width,
-      this.hallMonitor.height,
-      0xd94b4b
-    ).setStrokeStyle(3, 0x7a1f1f)
-
-    this.hallMonitorBadge = this.add.rectangle(
-      this.hallMonitor.x,
-      this.hallMonitor.y - 4,
-      10,
-      8,
-      0xf4d35e
-    )
-
-    this.hallMonitorLabel = this.add.text(
-      this.hallMonitor.x - 36,
-      this.hallMonitor.y - 34,
-      'Hall Monitor',
-      {
-        fontFamily: 'Arial',
-        fontSize: '14px',
-        color: '#4a1414'
-      }
-    )
+    this.hallMonitorShadow = this.add.ellipse(this.hallMonitor.x, this.hallMonitor.y + 17, 30, 12, 0x000000, 0.22)
+    this.hallMonitorBody = this.add.rectangle(this.hallMonitor.x, this.hallMonitor.y + 2, 24, 24, 0xd54c4c)
+      .setStrokeStyle(3, 0x731f1f)
+    this.hallMonitorHead = this.add.circle(this.hallMonitor.x, this.hallMonitor.y - 14, 10, 0xffd4b0)
+      .setStrokeStyle(2, 0x7c4d39)
+    this.hallMonitorCap = this.add.rectangle(this.hallMonitor.x, this.hallMonitor.y - 22, 20, 8, 0x7f2d2d)
+      .setStrokeStyle(2, 0x4e1818)
+    this.hallMonitorBadge = this.add.rectangle(this.hallMonitor.x + 5, this.hallMonitor.y + 1, 8, 8, 0xf4d35e)
+      .setStrokeStyle(1, 0x9b7b12)
+    this.hallMonitorClipboard = this.add.rectangle(this.hallMonitor.x - 8, this.hallMonitor.y + 4, 8, 14, 0xf7efcc)
+      .setAngle(-12)
+      .setStrokeStyle(1, 0x7b6c45)
+    this.hallMonitorLabel = this.add.text(this.hallMonitor.x - 40, this.hallMonitor.y - 46, 'Hall Monitor', {
+      fontFamily: 'Arial',
+      fontSize: '14px',
+      color: '#5c2020'
+    })
   }
 
   createPlayer() {
@@ -288,15 +307,16 @@ export default class Game extends Phaser.Scene {
       height: 26
     }
 
-    this.playerShadow = this.add.ellipse(this.player.x, this.player.y + 14, 24, 10, 0x000000, 0.22)
-
-    this.playerBody = this.add.rectangle(
-      this.player.x,
-      this.player.y,
-      this.player.width,
-      this.player.height,
-      0x3b82f6
-    ).setStrokeStyle(3, 0x163d7a)
+    this.playerShadow = this.add.ellipse(this.player.x, this.player.y + 17, 28, 12, 0x000000, 0.24)
+    this.playerBody = this.add.rectangle(this.player.x, this.player.y + 2, 22, 24, 0x3b82f6)
+      .setStrokeStyle(3, 0x163d7a)
+    this.playerHead = this.add.circle(this.player.x, this.player.y - 14, 10, 0xffd9b8)
+      .setStrokeStyle(2, 0x8a5c42)
+    this.playerHair = this.add.rectangle(this.player.x, this.player.y - 20, 18, 7, 0x5c3b22)
+    this.playerSatchel = this.add.rectangle(this.player.x + 7, this.player.y + 2, 8, 12, 0xd19a3f)
+      .setStrokeStyle(1, 0x7a5a1b)
+    this.playerFaceMarker = this.add.rectangle(this.player.x, this.player.y - 10, 10, 4, 0x163d7a)
+      .setOrigin(0.5)
 
     this.attackSwish = this.add.rectangle(this.player.x, this.player.y, 24, 14, 0xffffff, 0.4)
       .setStrokeStyle(2, 0x163d7a)
@@ -304,10 +324,30 @@ export default class Game extends Phaser.Scene {
   }
 
   createUi() {
-    this.scoreText = this.add.text(20, 86, 'Gold: 0 / 2', {
+    this.hudTitle = this.add.text(48, 26, 'Escape from Penwortham', {
+      fontFamily: 'Arial',
+      fontSize: '28px',
+      color: '#fff7dc'
+    })
+
+    this.hudSubtitle = this.add.text(48, 54, 'Collect gold, bonk the Hall Monitor, grab the key, then head for the door.', {
+      fontFamily: 'Arial',
+      fontSize: '14px',
+      color: '#d7edf7'
+    })
+
+    this.scoreBadge = this.add.rectangle(575, 34, 120, 34, 0xf6ecc0).setStrokeStyle(2, 0x92743b)
+    this.scoreText = this.add.text(525, 24, 'Gold: 0 / 2', {
       fontFamily: 'Arial',
       fontSize: '18px',
-      color: '#1a1a1a'
+      color: '#3b2b14'
+    })
+
+    this.keyBadge = this.add.rectangle(685, 34, 110, 34, 0xd8eef8).setStrokeStyle(2, 0x3e7998)
+    this.keyStatusText = this.add.text(646, 24, 'Key: Missing', {
+      fontFamily: 'Arial',
+      fontSize: '18px',
+      color: '#16435a'
     })
 
     this.messageBox = this.add.rectangle(400, 96, 10, 34, 0x111111, 0.72)
@@ -324,6 +364,8 @@ export default class Game extends Phaser.Scene {
       .setOrigin(0.5)
       .setDepth(21)
       .setVisible(false)
+
+    this.updateHud()
   }
 
   update(time, delta) {
@@ -372,8 +414,7 @@ export default class Game extends Phaser.Scene {
     this.updateHallMonitor(dt)
     this.updateAttack(delta)
 
-    this.playerBody.setPosition(this.player.x, this.player.y)
-    this.playerShadow.setPosition(this.player.x, this.player.y + 14)
+    this.updatePlayerLook()
 
     this.checkCoinPickup(this.coin, 'hasCoin')
     this.checkCoinPickup(this.coin2, 'hasCoin2')
@@ -437,10 +478,13 @@ export default class Game extends Phaser.Scene {
       this.hallMonitor.direction = -1
     }
 
-    this.hallMonitorBody.setPosition(this.hallMonitor.x, this.hallMonitor.y)
-    this.hallMonitorShadow.setPosition(this.hallMonitor.x, this.hallMonitor.y + 15)
-    this.hallMonitorBadge.setPosition(this.hallMonitor.x, this.hallMonitor.y - 4)
-    this.hallMonitorLabel.setPosition(this.hallMonitor.x - 36, this.hallMonitor.y - 34)
+    this.hallMonitorShadow.setPosition(this.hallMonitor.x, this.hallMonitor.y + 17)
+    this.hallMonitorBody.setPosition(this.hallMonitor.x, this.hallMonitor.y + 2)
+    this.hallMonitorHead.setPosition(this.hallMonitor.x, this.hallMonitor.y - 14)
+    this.hallMonitorCap.setPosition(this.hallMonitor.x, this.hallMonitor.y - 22)
+    this.hallMonitorBadge.setPosition(this.hallMonitor.x + 5, this.hallMonitor.y + 1)
+    this.hallMonitorClipboard.setPosition(this.hallMonitor.x - 8, this.hallMonitor.y + 4)
+    this.hallMonitorLabel.setPosition(this.hallMonitor.x - 40, this.hallMonitor.y - 46)
   }
 
   startAttack() {
@@ -521,7 +565,7 @@ export default class Game extends Phaser.Scene {
       this[flagName] = true
       coinShape.setVisible(false)
       this.score += 1
-      this.scoreText.setText(`Gold: ${this.score} / 2`)
+      this.updateHud()
       this.showMessage('Gold collected!')
     }
   }
@@ -541,6 +585,8 @@ export default class Game extends Phaser.Scene {
     if (distance < 28) {
       this.hasKey = true
       this.key.setVisible(false)
+      this.updateHud()
+      this.updateDoorLook()
       this.showMessage('You got the key!')
     }
   }
@@ -568,7 +614,10 @@ export default class Game extends Phaser.Scene {
     this.hallMonitorDefeated = true
     this.hallMonitorBody.setVisible(false)
     this.hallMonitorShadow.setVisible(false)
+    this.hallMonitorHead.setVisible(false)
+    this.hallMonitorCap.setVisible(false)
     this.hallMonitorBadge.setVisible(false)
+    this.hallMonitorClipboard.setVisible(false)
     this.hallMonitorLabel.setVisible(false)
     this.spawnKey(this.hallMonitor.x + 10, this.hallMonitor.y)
     this.showMessage('Hall Monitor defeated!')
@@ -606,8 +655,7 @@ export default class Game extends Phaser.Scene {
 
     this.player.x = this.playerStartX
     this.player.y = this.playerStartY
-    this.playerBody.setPosition(this.player.x, this.player.y)
-    this.playerShadow.setPosition(this.player.x, this.player.y + 14)
+    this.updatePlayerLook()
     this.showMessage('Tagged! Back to start.')
   }
 
@@ -635,7 +683,7 @@ export default class Game extends Phaser.Scene {
 
     if (this.hasKey) {
       this.hasExited = true
-      this.exitDoor.setFillStyle(0x9b6a38)
+      this.updateDoorLook()
       this.showMessage('Unlocked! Home time!')
     } else if (!this.wasTouchingExit) {
       this.showMessage('Door is locked.')
@@ -678,5 +726,74 @@ export default class Game extends Phaser.Scene {
       duration: 1000,
       ease: 'Sine.easeOut'
     })
+  }
+
+  updateHud() {
+    this.scoreText.setText(`Gold: ${this.score} / 2`)
+    this.keyStatusText.setText(this.hasKey ? 'Key: Found' : 'Key: Missing')
+    this.keyBadge.setFillStyle(this.hasKey ? 0xcff4d7 : 0xd8eef8)
+    this.keyBadge.setStrokeStyle(2, this.hasKey ? 0x2d7a41 : 0x3e7998)
+    this.keyStatusText.setColor(this.hasKey ? '#215b30' : '#16435a')
+  }
+
+  updateDoorLook() {
+    if (this.hasKey) {
+      this.exitFrame.setFillStyle(0x265b34)
+      this.exitDoor.setFillStyle(0x3ea35a)
+      this.exitDoor.setStrokeStyle(2, 0x1f5c31)
+      this.exitHandle.setFillStyle(0xfff1a8)
+      this.exitWindow.setFillStyle(0xcaf4ae, 0.9)
+      this.exitSign.setFillStyle(0xe0f4c9)
+      this.exitSignText.setText('OPEN')
+      this.exitSignText.setColor('#215b30')
+      this.exitSignText.setPosition(this.roomRight - 67, this.roomTop + 278)
+    } else {
+      this.exitFrame.setFillStyle(0x58341d)
+      this.exitDoor.setFillStyle(0x8b5a2b)
+      this.exitDoor.setStrokeStyle(2, 0x59371c)
+      this.exitHandle.setFillStyle(0xffd54f)
+      this.exitWindow.setFillStyle(0x8fc8e8, 0.8)
+      this.exitSign.setFillStyle(0xf8f2cf)
+      this.exitSignText.setText('LOCKED')
+      this.exitSignText.setColor('#6b2a2a')
+      this.exitSignText.setPosition(this.roomRight - 76, this.roomTop + 278)
+    }
+  }
+
+  updatePlayerLook() {
+    this.playerShadow.setPosition(this.player.x, this.player.y + 17)
+    this.playerBody.setPosition(this.player.x, this.player.y + 2)
+    this.playerHead.setPosition(this.player.x, this.player.y - 14)
+    this.playerHair.setPosition(this.player.x, this.player.y - 20)
+    this.playerSatchel.setPosition(this.player.x + 7, this.player.y + 2)
+
+    let markerX = this.player.x
+    let markerY = this.player.y - 10
+    let markerWidth = 10
+    let markerHeight = 4
+
+    if (this.playerFacing === 'left') {
+      markerX = this.player.x - 7
+      markerWidth = 4
+      markerHeight = 10
+      this.playerSatchel.setPosition(this.player.x + 7, this.player.y + 2)
+    } else if (this.playerFacing === 'right') {
+      markerX = this.player.x + 7
+      markerWidth = 4
+      markerHeight = 10
+      this.playerSatchel.setPosition(this.player.x - 7, this.player.y + 2)
+    } else if (this.playerFacing === 'up') {
+      markerY = this.player.y - 15
+      markerWidth = 10
+      markerHeight = 4
+    } else {
+      markerY = this.player.y - 6
+      markerWidth = 10
+      markerHeight = 4
+    }
+
+    this.playerFaceMarker.setPosition(markerX, markerY)
+    this.playerFaceMarker.setSize(markerWidth, markerHeight)
+    this.playerFaceMarker.setDisplaySize(markerWidth, markerHeight)
   }
 }
